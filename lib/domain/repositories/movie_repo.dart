a53d7daf2ece1aa1ebf4cc113ee6e626a2bbc8a0
Renan Martins <renanmdreis@alu.ufc.br>
2025-09-04 19:25:46 -0300

    feat (domain): create movie repository

---
+++ lib/domain/repositories/movie_repo.dart
@@ -0,0 +1,9 @@
+import 'package:serasa_challenge/domain/entities/movie_details.dart';
+import '../entities/movie.dart';
+
+abstract class MovieRepository {
+  Future<List<Movie>> searchMovies(String query);
+  Future<MovieDetails> getMovieDetails(String id);
+  Future<List<Movie>> getRecentMovies();
+  Future<void> saveRecentMovie(Movie movie);
+}
